62633b974fe3383b4cc4b919c963bec3eff39506
Marco Cesar da Silva <marcocesardasilva@gmail.com>
2024-11-15 02:53:18 -0300

    Modelos

--- predicao/1_query.sql
+++ predicao/1_query.sql
@@ -2,34 +2,34 @@
 -- Taxa média quadrimestral do yll
 --------------------------------------------------------------------------------------------------------------
 select
-  a.quadrimestre,
-  avg(a.taxa_yll) as taxa_media_yll
+	tx.quadrimestre,
+	avg(tx.taxa_yll) as taxa_media_yll
 from (
-with yll_quadrimestral as (
+	with yll_quadrimestral as (
+		select
+			case
+				when extract(month from y.dt_obito) between 1 and 4 then date(extract(year from y.dt_obito), 4, 30)
+				when extract(month from y.dt_obito) between 5 and 8 then date(extract(year from y.dt_obito), 8, 31)
+				when extract(month from y.dt_obito) between 9 and 12 then date(extract(year from y.dt_obito), 12, 31)
+				end as quadrimestre,
+			m.nm_municipio,
+			p.populacao,
+			sum(y.yll) as soma_yll
+		from `ml-na-saude.yll_por_obito.yll` y
+		join `ml-na-saude.yll_por_obito.populacao` p on y.cd_mun_res = p.cd_municipio and y.ano_obito = p.ano
+		join `ml-na-saude.yll_por_obito.municipio` m on p.cd_municipio = m.cd_municipio
+		where p.porte = 'Médio Porte'
+		group by 1,2,3
+	)
 	select
-		case
-			when extract(month from y.dt_obito) between 1 and 4 then date(extract(year from y.dt_obito), 4, 30)
-			when extract(month from y.dt_obito) between 5 and 8 then date(extract(year from y.dt_obito), 8, 31)
-			when extract(month from y.dt_obito) between 9 and 12 then date(extract(year from y.dt_obito), 12, 31)
-			end as quadrimestre,
-		m.nm_municipio,
-		p.populacao,
-		sum(y.yll) as soma_yll
-	from `ml-na-saude.yll_por_obito.yll` y
-	join `ml-na-saude.yll_por_obito.populacao` p on y.cd_mun_res = p.cd_municipio and y.ano_obito = p.ano
-	join `ml-na-saude.yll_por_obito.municipio` m on p.cd_municipio = m.cd_municipio
-	where p.porte = 'Médio Porte'
-    group by 1,2,3
-)
-select
-	quadrimestre,
-	nm_municipio,
-  soma_yll,
-  populacao,
-  soma_yll / populacao * 1000 as taxa_yll
-from yll_quadrimestral
-group by 1,2,3,4
-) a
+		quadrimestre,
+		nm_municipio,
+		soma_yll,
+		populacao,
+		soma_yll / populacao * 1000 as taxa_yll
+	from yll_quadrimestral
+	group by 1,2,3,4
+) tx
 group by 1
 order by 1
 ;
